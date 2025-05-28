29bb3fa2971c05249b5e6e7570fb9dae09a28797
martin-sharafutdinov@mail.ru <martin-sharafutdinov@mail.ru>
2025-05-28 21:13:20 +0300

    по русски

--- lab3/pages/main/index.js
+++ lab3/pages/main/index.js
@@ -65,16 +65,80 @@ export class MainPage {
 
     getData() {
         return [
-          { id: 1, src: "https://cdna.artstation.com/p/assets/images/images/018/773/854/large/shin-jong-hun-asdasf.jpg?1560684630", title: "Elule", author: "Shin Jong Hun", date: "2019-06-16", description: "A magical character painted in a fantasy setting.", likes: 12 },
-          { id: 2, src: "https://cdnb.artstation.com/p/assets/images/images/045/336/111/large/lorenzo-lanfranconi-painting-san-donato-3.jpg?1642491687", title: "Walk to San Donato", author: "Lorenzo Lanfranconi", date: "2022-01-18", description: "A serene landscape in traditional style.", likes: 5 },
-          { id: 3, src: "https://cdna.artstation.com/p/assets/images/images/034/605/970/large/shin-jong-hun-1612619763114.jpg?1612746686", title: "Mother Nature", author: "Shin Jong Hun", date: "2021-02-08", description: "Places I want to go...", likes: 6 },
-          { id: 4, src: "https://cdna.artstation.com/p/assets/images/images/019/693/026/large/wangjie-li-apex-bangalore.jpg?1564605666", title: "Bangalore", author: "Wangjie Li", date: "2019-06-25", description: "Illustration and sketches of Bangalore from Apex...", likes: 17 },
-          { id: 5, src: "https://cdnb.artstation.com/p/assets/images/images/033/511/717/large/finnian-macmanus-aroth1.jpg?1609874157", title: "Machines of Eroth", author: "Finnian MacManus", date: "2021-08-03", description: "A city of clockwork machinery...", likes: 10 },
-          { id: 6, src: "https://cdnb.artstation.com/p/assets/images/images/018/724/051/large/bo-chen-dark-cosmic-jhin-final-splash-1920.jpg?1560454527", title: "Dark Cosmic Jhin", author: "Bo Chen", date: "2019-02-12", description: "He is kind of between Dark Star and Cosmic...", likes: 30 },
-          { id: 7, src: "https://cdnb.artstation.com/p/assets/images/images/026/594/027/large/terence-cantal-queenfrozen-finale3.jpg?1660232512", title: "Frozen Queen", author: "Terence CANTAL", date: "2020-05-07", description: "This was a challenge to myself...", likes: 12 },
-          { id: 8, src: "https://cdna.artstation.com/p/assets/images/images/055/128/704/large/sylvain-sarrailh-cypress-cliff.jpg?1666189543", title: "The Cypress Cliff", author: "Sylvain Sarrailh", date: "2023-04-15", description: "Illustration made for the exhibition Art Ex Machina at Toulouse.", likes: 7 }
-        ];
-      }
+            {
+                id: 1,
+                src: "https://cdna.artstation.com/p/assets/images/images/018/773/854/large/shin-jong-hun-asdasf.jpg?1560684630",
+                title: "Элуна",
+                author: "Шин Чон Хун",
+                date: "2019-06-16",
+                description: "Волшебный персонаж, изображенный в фэнтезийной обстановке.",
+                likes: 5
+            },
+            {
+                id: 2,
+                src: "https://cdnb.artstation.com/p/assets/images/images/045/336/111/large/lorenzo-lanfranconi-painting-san-donato-3.jpg?1642491687",
+                title: "Прогулка до Сан-Донато",
+                author: "Лоренцо Ланфранкони",
+                date: "2022-01-18",
+                description: "Спокойный пейзаж в традиционном стиле.",
+                likes: 3
+            },
+            {
+                id: 3,
+                src: "https://cdna.artstation.com/p/assets/images/images/034/605/970/large/shin-jong-hun-1612619763114.jpg?1612746686",
+                title: "Мать природа",
+                author: "Шин Чон Хун",
+                date: "2021-02-08",
+                description: "Места, куда я хочу поехать. Сочетание освежающего зеленого, синего и белого. Природа всегда прекрасна.",
+                likes: 7
+            },
+            {
+                id: 4,
+                src: "https://cdna.artstation.com/p/assets/images/images/019/693/026/large/wangjie-li-apex-bangalore.jpg?1564605666",
+                title: "Бангалор",
+                author: "Вангджи Ли",
+                date: "2019-06-25",
+                description: "Иллюстрация и наброски Бангалора от Apex. Я рад, что могу работать над этой игрой, она потрясающая!",
+                likes: 8
+            },
+            {
+                id: 5,
+                src: "https://cdnb.artstation.com/p/assets/images/images/033/511/717/large/finnian-macmanus-aroth1.jpg?1609874157",
+                title: "Машины Эрота",
+                author: "Финниан Мак Манус",
+                date: "2021-08-03",
+                description: "Город заводных механизмов, который давно замерз.",
+                likes: 3
+            },
+            {
+                id: 6,
+                src: "https://cdnb.artstation.com/p/assets/images/images/018/724/051/large/bo-chen-dark-cosmic-jhin-final-splash-1920.jpg?1560454527",
+                title: "Тёмный Космический Джин",
+                author: "Бо Чен",
+                date: "2019-02-12",
+                description: "Он что-то среднее между Темной Звездой и Космиком. Он разрушает и создает одновременно. Бог, который так увлечен своими собственными делами. Он идет по Млечному Пути, поглощая энергию для своего оружия и сокрушая планеты своими пулями.",
+                likes: 15
+            },
+            {
+                id: 7,
+                src: "https://cdnb.artstation.com/p/assets/images/images/026/594/027/large/terence-cantal-queenfrozen-finale3.jpg?1660232512",
+                title: "Замороженная королева",
+                author: "Теренс Кантал",
+                date: "2020-05-07",
+                description: "Это был вызов для меня, где я пыталась играть с контрастами истории. Холод окружающей среды и теплота взаимодействия матери и ребенка. Жестокость произошедшего по сравнению с мягкостью выражений главных героев. И так далее. Мне бы хотелось уделить больше времени этой иллюстрации. (Кроме того, анатомия малышей — это нечто особенное, и в ней сложно разобраться!!) Спасибо, что зашли.",
+                likes: 13
+            },
+            {
+                id: 8,
+                src: "https://cdna.artstation.com/p/assets/images/images/055/128/704/large/sylvain-sarrailh-cypress-cliff.jpg?1666189543",
+                title: "Кипарисовый утёс",
+                author: "Сильвен Саррайх",
+                date: "2023-04-15",
+                description: "Иллюстрация, выполненная для выставки Art Ex Machina в Тулузе.",
+                likes: 5
+            }
+        ]
+    }
     
 
     render() {
